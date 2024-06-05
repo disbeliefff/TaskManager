e58a0c8ad8ca85c317e2aa7e7ff66171054c8e2a
disbeliefff <sergeycrowler@gmail.com>
2024-06-05 16:31:05 +0300

    Aaaa

--- src/main/java/com/myprojects/todo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/myprojects/todo/service/impl/TaskServiceImpl.java
@@ -1,35 +1,60 @@
 package com.myprojects.todo.service.impl;
 
+import com.myprojects.todo.domain.task.Status;
 import com.myprojects.todo.domain.task.Task;
+import com.myprojects.todo.exception.ResourceNotFoundException;
+import com.myprojects.todo.repository.TaskRepository;
+import com.myprojects.todo.repository.UserRepository;
 import com.myprojects.todo.service.TaskService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class TaskServiceImpl implements TaskService {
+
+    private final TaskRepository taskRepository;
+
+
     @Override
+    @Transactional(readOnly = true)
     public Task getById(Long id) {
-        return null;
+        return taskRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Task not found"));
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Task> getAllByUserId(Long id) {
-        return null;
+        return taskRepository.findAllByUserId(id);
     }
 
     @Override
+    @Transactional
     public Task update(Task task) {
-        return null;
+        if (task.getId() == null) {
+            task.setStatus(Status.TODO);
+        }
+        taskRepository.update(task);
+        return task;
     }
 
     @Override
-    public Task create(Task task, Long id) {
-        return null;
+    @Transactional
+    public Task create(Task task, Long userId) {
+        task.setStatus(Status.TODO);
+        taskRepository.create(task);
+        taskRepository.assignToUserById(task.getId(), userId);
+        return task;
+
     }
 
     @Override
+    @Transactional
     public void delete(Long id) {
-
+        taskRepository.delete(id);
     }
 }
--- src/main/java/com/myprojects/todo/service/impl/UserServiceImpl.java
+++ src/main/java/com/myprojects/todo/service/impl/UserServiceImpl.java
@@ -1,39 +1,66 @@
 package com.myprojects.todo.service.impl;
 
 import com.myprojects.todo.domain.user.User;
+import com.myprojects.todo.exception.ResourceNotFoundException;
+import com.myprojects.todo.repository.UserRepository;
 import com.myprojects.todo.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
     @Override
+    @Transactional(readOnly = true)
     public User getById(Long id) {
-        return null;
+        return userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
     }
 
     @Override
+    @Transactional(readOnly = true)
     public User getByUsername(String username) {
-        return null;
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException("Can't find user with username " + username));
     }
 
     @Override
+    @Transactional
     public User update(User user) {
-        return null;
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.update(user);
+        return user;
     }
 
     @Override
+    @Transactional
     public User create(User user) {
-        return null;
+        if (userRepository.findByUsername(user.getUsername()).isPresent()) {
+            throw new IllegalStateException("User already exists");
+        }
+        if (!user.getPassword().equals(user.getPasswordConfirmation())) {
+            throw new IllegalStateException("Passwords do not match");
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.create(user);
+        return user;
     }
 
     @Override
+    @Transactional(readOnly = true)
     public boolean isTaskOwner(Long userId, Long taskId) {
-        return false;
+        return userRepository.isTaskOwner(userId, taskId);
     }
 
     @Override
+    @Transactional
     public void delete(Long id) {
-
+        userRepository.delete(id);
     }
 }
